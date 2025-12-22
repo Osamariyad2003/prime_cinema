1510f773fdf7edd5e26e9653e8462a49b61cd9fc
Osamariyad2003 <92514554+Osamariyad2003@users.noreply.github.com>
2025-12-22 03:38:30 +0300

    Update routing and homepage URL in package.json and App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/prime_cinema/">
       <App />
     </BrowserRouter>
   </StrictMode>,
